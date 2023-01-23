b3a4a8fc052569e47401c43ebd425d8a2dacca92
Maksim Akimychev <101060239+akimychau@users.noreply.github.com>
2023-01-23 22:56:28 +0100

    Урок 4 | конвертируем картинку в PNG и сохраняем в файловую систему телефона

--- app/src/main/java/ru/akimychev/convertjpgtopng/ImagePicker.kt
+++ app/src/main/java/ru/akimychev/convertjpgtopng/ImagePicker.kt
@@ -1,11 +1,21 @@
 package ru.akimychev.convertjpgtopng
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class ImagePicker(
+    private val converter: ConverterJpgToPng,
     registry: ActivityResultRegistry,
     callback: (imageUri: Uri?) -> Unit
 ) {
@@ -17,7 +27,39 @@ class ImagePicker(
         getContent.launch("image/*")
     }
 
+    fun convert(uri: Uri?) {
+        converter.convertRx(uri)
+    }
+
     private companion object {
         const val RESULT_REGISTRY_KEY = "pick_image"
+    }
+}
+
+class ConverterJpgToPng(private val currentContext: Context) {
+    fun convertRx(uri: Uri?) {
+        uri?.let {
+            val externalStorageState = Environment.getExternalStorageState()
+            if (externalStorageState.equals(Environment.MEDIA_MOUNTED)) {
+                val storageDirectory =
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                val file = File(storageDirectory, "${System.currentTimeMillis()}.png")
+                try {
+                    val stream: OutputStream = FileOutputStream(file)
+                    val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        ImageDecoder.decodeBitmap(
+                            ImageDecoder.createSource(currentContext.contentResolver, it)
+                        )
+                    } else {
+                        MediaStore.Images.Media.getBitmap(currentContext.contentResolver, it)
+                    }
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    stream.flush()
+                    stream.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/ru/akimychev/convertjpgtopng/MainActivity.kt
+++ app/src/main/java/ru/akimychev/convertjpgtopng/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.akimychev.convertjpgtopng
 
 import android.Manifest
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
@@ -9,12 +10,16 @@ import androidx.appcompat.app.AppCompatActivity
 import coil.load
 import ru.akimychev.convertjpgtopng.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val imagePicker = ImagePicker(activityResultRegistry) { imageUri ->
-        binding.img.load(imageUri)
-    }
+    private var uri: Uri? = null
+    private val imagePicker =
+        ImagePicker(ConverterJpgToPng(this), activityResultRegistry) { imageUri ->
+            binding.img.load(imageUri)
+            uri = imageUri
+        }
 
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
@@ -22,7 +27,7 @@ class MainActivity : AppCompatActivity() {
                 granted -> {
                     imagePicker.selectImage()
                 }
-                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
                     showDialogForRequestPermission()
                 }
                 else -> {
@@ -41,10 +46,15 @@ class MainActivity : AppCompatActivity() {
         binding.btn.setOnClickListener {
             requestPermission()
         }
+
+        binding.btn2.setOnClickListener {
+
+            imagePicker.convert(uri)
+        }
     }
 
     private fun requestPermission() {
-        requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
 
     private fun showDialogForRequestPermission() {
